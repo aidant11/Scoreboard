be6bfc20171e52d7e8b8c12bb456d5eeda899f87
aidant11 <aidant10@nycstudents.net>
2024-10-07 15:50:39 +0000

    123

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main
         Scoreboard game = new Scoreboard ("Red", "Blue");
         info = game.getScore();
         System.out.println(info);
-        game.recordPlay(1);
+        game.recordPlay(0);
         info = game.getScore();
         System.out.println(info);
     }
--- Scoreboard.java
+++ Scoreboard.java
@@ -18,7 +18,13 @@ public String getScore()
 }
 public void recordPlay(int score)
 {
-    
+    if(teamOne.equals(activeTeam)) {
+        teamOneScore += score;
+}
+  if(score == 0)
+        {
+            if(activeTeam.equals(teamOne)) activeTeam = teamTwo;
+            else if(activeTeam.equals(teamTwo)) activeTeam = teamOne;
+        }
 }
 }
-
